a4e61468ca41b3284f14201001ef7492a26fa0e5
orest1983 <orest1983@mail.ru>
2017-10-22 20:01:17 +0300

    Добавлена интерактивная карта

--- js/script.js
+++ js/script.js
@@ -1,3 +1,33 @@
+ymaps.ready(function () {
+  var myMap = new ymaps.Map('ya-map', {
+      center: [59.936550, 30.32080],
+      zoom: 16,
+      controls: []
+    },
+    {
+      suppressMapOpenBlock: true
+    },
+    {
+      searchControlProvider: 'yandex#search'
+    }),
+    myPlacemark = new ymaps.Placemark(([59.936489, 30.32080]),{},
+    {
+      iconLayout: 'default#image',
+      iconImageHref: 'img/icon-map-marker.svg',
+      iconImageSize: [36, 36],
+      iconImageOffset: [0, 0]
+    });
+  myMap.geoObjects.add(myPlacemark);
+  myMap.controls.remove('rulerControl');
+  myMap.controls.remove('searchControl');
+  myMap.controls.remove('trafficControl');
+  myMap.controls.remove('typeSelector');
+  myMap.controls.remove('zoomControl');
+  myMap.controls.remove('geolocationControl');
+  myMap.controls.remove('routeEditor');
+});
+
+// menu
 (function() {
   var pageHader = document.querySelector(".page-header");
   var toggle = document.querySelector(".main-nav__toggle");
